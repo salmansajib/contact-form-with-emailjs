55e106ea8037760cf61f5e2e1d1c9d44e58bcb0a
Salman-sajib <sfsajib@gmail.com>
2024-08-22 00:36:24 +0600

    added the email js

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function App() {
   const formRef = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_i5dspqn", "template_4kht7uf", formRef.current, {
+        publicKey: "4saBg4D-6Ec-jCnD9",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
-    <div className="min-h-screen bg-gradient-to-br from-green-200 to-blue-300 flex flex-col items-center justify-center text-gray-700 px-3">
+    <div className="min-h-screen bg-gradient-to-br from-green-200 to-cyan-400 flex flex-col items-center justify-center text-gray-700 px-3">
       <h1 className="text-3xl font-semibold">Contact Me</h1>
 
       <form
         ref={formRef}
-        className="text-inherit w-full max-w-[400px] mt-10 space-y-4"
+        onSubmit={sendEmail}
+        className="text-inherit w-full max-w-[400px] mt-10 space-y-2"
       >
         <div className="flex flex-col gap-1">
           <label className="font-medium cursor-pointer" htmlFor="name">
             Name
           </label>
           <input
-            className=" bg-transparent px-2 py-2 rounded-md border border-gray-100 outline-gray-100 placeholder:text-gray-400"
+            className=" bg-gray-100 px-2 py-2 rounded-md border border-none outline-gray-500 placeholder:text-gray-400"
             type="text"
             id="name"
             name="name"
@@ -29,7 +48,7 @@ function App() {
             Email
           </label>
           <input
-            className=" bg-transparent px-2 py-2 rounded-md border border-gray-100 outline-gray-100 placeholder:text-gray-400"
+            className=" bg-gray-100 px-2 py-2 rounded-md border border-none outline-gray-500 placeholder:text-gray-400"
             type="email"
             id="email"
             name="email"
@@ -45,7 +64,7 @@ function App() {
             style={{
               resize: "none",
             }}
-            className=" bg-transparent px-2 py-2 rounded-md border border-gray-100 outline-gray-100 placeholder:text-gray-400"
+            className=" bg-gray-100 px-2 py-2 rounded-md border border-none outline-gray-500 placeholder:text-gray-400"
             name="message"
             id="message"
             placeholder="type your message here"
